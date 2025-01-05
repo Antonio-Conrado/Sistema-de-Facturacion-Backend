3ed3b4b01c8c8ea17a0607139a168fefea732533
Antonio <jconradolopez19@gmail.com>
2025-01-05 07:53:50 -0600

    Environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import dotenv from 'dotenv'
 import routes from './routes'
 import testConnectionDB from './config/db'
 import { corsConfig } from './config/cors'
 
 const app = express()
+dotenv.config()
 app.use(morgan('dev'))
 app.use(cors(corsConfig))
 
