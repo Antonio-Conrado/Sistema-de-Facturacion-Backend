1f8265ad9b7e8d3a59575cf26fceee16bd40715e
Antonio <jconradolopez19@gmail.com>
2025-01-05 07:51:32 -0600

    nodemailer and send email

---
+++ src/config/nodemailer.ts
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer'
+
+const config = () => {
+    return {
+        host: process.env.EMAIL_HOST,
+        port: +process.env.EMAIL_PORT,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+        tls: {
+            ciphers: "TLSv1.2,TLSv1.3",
+        },
+    }
+}
+export const transport = nodemailer.createTransport(config());
---
+++ src/emails/AuthEmail.ts
@@ -0,0 +1,37 @@
+import { transport } from "../config/nodemailer"
+
+type EmailType = {
+    name: string
+    email: string
+    token: string
+}
+
+export class AuthEmail {
+    static sendConfirmatioEmail = async (user: EmailType) => {
+        await transport.sendMail({
+            from: 'CashTracker <admin@crashtracker.com',
+            to: user.email,
+            subject: 'crachtracker - confirma tu cuenta',
+            html: `
+                <p>Hola: ${user.name}, has creado tu cuenta</p>
+                <p>Haz click en el siguiente enlace para confirmar tu cuenta </p>
+                <a href='#'>Confirmar cuenta</a>
+                <p>Ingresa el codigo: <b>${user.token}</b></p>
+            `
+        })
+    }
+
+    static sendPasswordResetToken = async (user: EmailType) => {
+        await transport.sendMail({
+            from: 'CashTracker <admin@crashtracker.com',
+            to: user.email,
+            subject: 'crachtracker - restablece tu password',
+            html: `
+                <p>Hola: ${user.name}, has solicitado restablecer tu cuenta</p>
+                <p>Haz click en el siguiente enlace para restablecer tu passwor </p>
+                <a href='#'>Restablecer password</a>
+                <p>Ingresa el codigo: <b>${user.token}</b></p>
+            `
+        })
+    }
+}
