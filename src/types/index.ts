5a6d2010c82f2ea2b46acabed29ba9d60d7956f4
Antonio <jconradolopez19@gmail.com>
2025-01-05 07:52:19 -0600

    types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type User = {
+    id: number;
+    roleId: number;
+    name: string;
+    surname: string;
+    telephone: string | null;
+    email: string;
+    password: string;
+    image: string | null;
+    token: string | null;
+    isConfirm: boolean;
+    status: boolean;
+}
+
+export type UserData = Pick<User, 'id' | 'email' | 'roleId'>
