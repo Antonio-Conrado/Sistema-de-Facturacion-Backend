cb7d10a7a3988011ccba82382332a2f28b51fdeb
Antonio <jconradolopez19@gmail.com>
2025-02-07 17:16:35 -0600

    updated code formatting

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,36 +1,38 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import cloudinary from '../config/cloudinary';
 import { v4 as uuid } from 'uuid';
 import formidable from 'formidable';
-import { prisma } from '../config/db'
+import { prisma } from '../config/db';
 import { User } from '../types';
 import { hashPassword } from '../utils/auth';
 
 export class UserController {
     static createUser = async (req: Request, res: Response) => {
-        const { email } = req.body
+        const { email } = req.body;
         try {
-            const userExists = await prisma.users.findUnique({ where: { email } });
+            const userExists = await prisma.users.findUnique({
+                where: { email },
+            });
             if (userExists) {
-                res.status(409).json({ error: "El usuario ya existe" })
-                return
+                res.status(409).json({ error: 'El usuario ya existe' });
+                return;
             }
-            const user: User = req.body
-            user.password = await hashPassword(req.body.password)
-            user.isConfirm = true
-            await prisma.users.create({ data: user })
+            const user: User = req.body;
+            user.password = await hashPassword(req.body.password);
+            user.isConfirm = true;
+            await prisma.users.create({ data: user });
 
-            res.status(200).json({ msg: 'Usuario creado con éxito' })
+            res.status(200).json({ msg: 'Usuario creado con éxito' });
         } catch (error) {
-            res.status(500).json({ error: 'Hubo un error' })
+            res.status(500).json({ error: 'Hubo un error' });
         }
-    }
+    };
 
     static getAllUsers = async (req: Request, res: Response) => {
         try {
             const users = await prisma.users.findMany({
                 where: {
-                    isConfirm: true
+                    isConfirm: true,
                 },
                 select: {
                     id: true,
@@ -38,76 +40,90 @@ export class UserController {
                     surname: true,
                     email: true,
                     roles: {
-                        select: { name: true }
-                    }
-                }
-            })
-            res.status(200).json(users)
+                        select: { name: true },
+                    },
+                },
+            });
+            res.status(200).json(users);
         } catch (error) {
-            res.status(500).json({ error: 'Hubo un error' })
+            res.status(500).json({ error: 'Hubo un error' });
         }
-    }
+    };
 
     static getUser = async (req: Request, res: Response) => {
-        const id = parseInt(req.params.userId, 10)
+        const id = parseInt(req.params.userId, 10);
         try {
-            const user = await prisma.users.findUnique({ where: { id } });
-            res.status(200).json(user)
+            const user = await prisma.users.findUnique({
+                where: { id },
+                select: {
+                    id: true,
+                    image: true,
+                    name: true,
+                    surname: true,
+                    roleId: true,
+                    email: true,
+                    telephone: true,
+                    roles: {
+                        select: { name: true },
+                    },
+                },
+            });
+            res.status(200).json(user);
         } catch (error) {
-            res.status(500).json({ error: 'Hubo un error' })
+            res.status(500).json({ error: 'Hubo un error' });
         }
-    }
+    };
 
     static updateUser = async (req: Request, res: Response) => {
-        const id = parseInt(req.params.userId, 10)
+        const id = parseInt(req.params.userId, 10);
         try {
             const userExists = await prisma.users.findFirst({
                 where: {
                     email: req.body.email,
-                    NOT: { id }
-                }
+                    NOT: { id },
+                },
             });
             if (userExists) {
                 res.status(409).json({ error: 'Email no disponible' });
                 return;
             }
 
-            req.body.password = await hashPassword(req.body.password)
+            req.body.password = await hashPassword(req.body.password);
             await prisma.users.update({
                 where: { id },
-                data: req.body
-            })
-            res.status(200).json({ msg: 'Usuario actualizado éxitosamente' })
+                data: req.body,
+            });
+            res.status(200).json({ msg: 'Usuario actualizado éxitosamente' });
         } catch (error) {
-            console.log(error)
-            res.status(500).json({ error: 'Hubo un error' })
+            console.log(error);
+            res.status(500).json({ error: 'Hubo un error' });
         }
-    }
+    };
 
     static suspendUser = async (req: Request, res: Response) => {
-        const id = parseInt(req.params.userId, 10)
+        const id = parseInt(req.params.userId, 10);
         try {
             await prisma.users.update({
                 where: { id },
-                data: { status: !req.user.status }
-            })
+                data: { status: !req.user.status },
+            });
 
-            res.status(200).json({ msg: 'Usuario actualizado éxitosamente' })
+            res.status(200).json({ msg: 'Usuario actualizado éxitosamente' });
         } catch (error) {
-            res.status(500).json({ error: 'Hubo un error' })
+            res.status(500).json({ error: 'Hubo un error' });
         }
-    }
+    };
 
     static uploadImageUser = async (req: Request, res: Response) => {
-        const id = parseInt(req.params.userId, 10)
+        const id = parseInt(req.params.userId, 10);
         try {
             const user = await prisma.users.update({
                 where: { id },
-                data: { image: req.image }
-            })
-            res.status(200).json({ image : user.image})
+                data: { image: req.image },
+            });
+            res.status(200).json({ image: user.image });
         } catch (error) {
-            res.status(500).json({ error: 'Hubo un error' })
+            res.status(500).json({ error: 'Hubo un error' });
         }
-    }
+    };
 }
