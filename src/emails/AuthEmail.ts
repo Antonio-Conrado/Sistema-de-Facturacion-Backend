2955faa1b985517abb5b8530e9e607871646a772
Antonio <jconradolopez19@gmail.com>
2025-02-07 17:10:41 -0600

    Updated password reset URL in email template

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -29,7 +29,7 @@ export class AuthEmail {
             html: `
                 <p>Hola: ${user.name}, has solicitado restablecer tu cuenta</p>
                 <p>Haz click en el siguiente enlace para restablecer tu passwor </p>
-                <a href='${process.env.FRONTEND_URL}/resetear-password/${user.token}'>Restablecer password</a>
+                <a href='${process.env.FRONTEND_URL}/resetear-password'>Restablecer password</a>
                 <p>Ingresa el codigo: <b>${user.token}</b></p>
             `
         })
