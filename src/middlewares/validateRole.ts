71aeafb5b38ca6e5eaf720e317d1c5362f72c9d9
Antonio <jconradolopez19@gmail.com>
2025-02-07 17:09:29 -0600

    validate roles

--- src/middlewares/validateRole.ts
+++ src/middlewares/validateRole.ts
@@ -1,18 +1,18 @@
-import { Request, Response, NextFunction } from "express";
-import { prisma } from "../config/db";
-export const validateRole = (role: string) => {
+import { Request, Response, NextFunction } from 'express';
+import { prisma } from '../config/db';
+export const validateRole = (roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         const user = await prisma.users.findUnique({
             where: { id: req.user.id },
             include: {
-                roles: true
-            }
-        })
-        if (user.roles.name !== role) {
-            const error = new Error('Rol no autorizado')
-            res.status(401).json({ error: error.message })
-            return
+                roles: true,
+            },
+        });
+        if (!roles.includes(user.roles.name)) {
+            const error = new Error('Rol no autorizado');
+            res.status(401).json({ error: error.message });
+            return;
         }
-        next()
-    }
-}
+        next();
+    };
+};
