f23689cc8e8cb930fe4efb012cc707c3580a99d2
Antonio <jconradolopez19@gmail.com>
2025-02-11 15:59:27 -0600

    added middleware for validate input

--- src/middlewares/userValidation.ts
+++ src/middlewares/userValidation.ts
@@ -1,55 +1,85 @@
-import { Request, Response, NextFunction } from "express";
-import { prisma } from "../config/db";
-import { body, param, validationResult } from "express-validator";
-import { UserData } from "../types";
+import { Request, Response, NextFunction } from 'express';
+import { prisma } from '../config/db';
+import { body, param, validationResult } from 'express-validator';
+import { UserData } from '../types';
 
 declare global {
     namespace Express {
         interface Request {
-            user?: UserData
+            user?: UserData;
         }
     }
 }
 
-export const validateUserExists = async (req: Request, res: Response, next: NextFunction) => {
+export const validateUserExists = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     try {
         const id = parseInt(req.params.userId, 10);
-        const user = await prisma.users.findUnique({ where: { id } })
+        const user = await prisma.users.findUnique({ where: { id } });
         if (!user) {
-            res.status(404).json({ error: 'El usuario no existe' })
-            return
+            res.status(404).json({ error: 'El usuario no existe' });
+            return;
         }
-        req.user = user
+        req.user = user;
         next();
     } catch (error) {
-        res.status(500).json({ error: 'Hubo un error' })
+        res.status(500).json({ error: 'Hubo un error' });
     }
-}
+};
 
-export const validateUserId = async (req: Request, res: Response, next: NextFunction) => {
+export const validateUserId = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     await param('userId')
-        .custom(value => value > 0).withMessage('El id del usuario no es válido').run(req)
+        .custom((value) => value > 0)
+        .withMessage('El id del usuario no es válido')
+        .run(req);
 
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() })
-        return
+        res.status(400).json({ errors: errors.array() });
+        return;
     }
     next();
-}
+};
 
-export const validateUserInputs = async (req: Request, res: Response, next: NextFunction) => {
+export const validateUserInputs = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     await body('name')
-        .notEmpty().withMessage('El nombre es obligatorio').run(req)
+        .notEmpty()
+        .withMessage('El nombre es obligatorio')
+        .run(req);
     await body('surname')
-        .notEmpty().withMessage('El apellido es obligatorio').run(req)
-    await body('email')
-        .isEmail().withMessage('El email no es válido').run(req)
-    await body('password')
-        .notEmpty().withMessage('El password es obligatorio')
-        .isLength({ min: 6 }).withMessage('El password debe tener mínimo 6 carácteres').run(req)
+        .notEmpty()
+        .withMessage('El apellido es obligatorio')
+        .run(req);
+    await body('email').isEmail().withMessage('El email no es válido').run(req);
     await body('roleId')
-        .notEmpty().withMessage('El rol es obligatorio').run(req)
+        .notEmpty()
+        .withMessage('El rol es obligatorio')
+        .run(req);
 
-    next()
-}
+    next();
+};
+
+export const validateUserInputPassword = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    await body('password')
+        .notEmpty()
+        .withMessage('El password es obligatorio')
+        .isLength({ min: 6 })
+        .withMessage('El password debe tener mínimo 6 carácteres')
+        .run(req);
+    next();
+};
