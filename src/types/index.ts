54203ea451c7a4cf72b48b059d708fec5f2fbf9e
Antonio <jconradolopez19@gmail.com>
2025-03-09 10:29:33 -0600

    types

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,6 +10,28 @@ export type User = {
     token: string | null;
     isConfirm: boolean;
     status: boolean;
-}
+};
 
-export type UserData = Pick<User, 'id' | 'email' | 'status' | 'roleId'>
+export type UserData = Pick<User, 'id' | 'email' | 'status' | 'roleId'>;
+
+export type Product = {
+    id?: number;
+    code: string;
+    name: string;
+    categoriesId: number;
+    status?: boolean;
+};
+export type DetailsProducts = {
+    id?: number;
+    description?: string;
+    image?: string;
+    products: Product;
+};
+export type StoredProducts = {
+    id?: number;
+    stock: number;
+    purchasePrice: number;
+    salePrice: number;
+    status?: boolean;
+    detailsProducts: DetailsProducts;
+};
