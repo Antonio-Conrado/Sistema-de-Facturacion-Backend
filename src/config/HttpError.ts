66084cd1985d9b820a8b5bc5ffcf6942ceb2d891
Antonio <jconradolopez19@gmail.com>
2025-03-10 08:55:41 -0600

    implemented HttpError class and catchErrors function for better error handling

---
+++ src/config/HttpError.ts
@@ -0,0 +1,20 @@
+import { Response } from 'express';
+export class HttpError extends Error {
+    statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.name = this.constructor.name;
+    }
+}
+
+export const catchErrors = (res: Response, error) => {
+    if (error.statusCode === 404) {
+        return res.status(404).json({ error: error.message });
+    }
+    if (error.statusCode === 409) {
+        return res.status(409).json({ error: error.message });
+    }
+    res.status(500).json('Hubo un error. Intente nuevamente.');
+};
