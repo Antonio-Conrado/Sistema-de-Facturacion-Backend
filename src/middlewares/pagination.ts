1f79952739694575451b63718e102cdafdac528f
Antonio <jconradolopez19@gmail.com>
2025-04-06 16:11:26 -0600

    feat(pagination): add middleware to validate pagination query parameters

--- src/middlewares/pagination.ts
+++ src/middlewares/pagination.ts
@@ -1,21 +1,48 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
+import { query } from 'express-validator';
 type Pagination = {
-    page: number
-    limit: number
-}
+    page: number;
+    limit: number;
+};
 declare global {
     namespace Express {
         interface Request {
-            pagination?: Pagination
+            pagination?: Pagination;
         }
     }
 }
 
-export const validatePagination = async (req: Request, res: Response, next: NextFunction) => {
-    const page = parseInt(req.query.page as string, 10) || 1
-    const limit = parseInt(req.query.limit as string, 10) || 12
-    
-    const pagination: Pagination = {page, limit}
-    req.pagination = pagination
+export const validatePagination = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const page = parseInt(req.query.page as string, 10) || 1;
+    const limit = parseInt(req.query.limit as string, 10) || 12;
+
+    const pagination: Pagination = { page, limit };
+    req.pagination = pagination;
     next();
-}
+};
+
+export const validatePaginationQuery = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    await query('take')
+        .optional()
+        .toInt()
+        .isInt()
+        .withMessage('El límite de datos por página debe ser un número entero')
+        .run(req);
+
+    await query('skip')
+        .optional()
+        .toInt()
+        .isInt()
+        .withMessage('El salto de datos por página debe ser un número entero')
+        .run(req);
+
+    next();
+};
