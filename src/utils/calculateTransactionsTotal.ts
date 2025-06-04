8de4875d96a6a8c96d1c9031bc56a49186643b68
Antonio-Conrado <jconradolopez19@gmail.com>
2025-06-04 16:13:47 -0600

    calculate iva with value

--- src/utils/calculateTransactionsTotal.ts
+++ src/utils/calculateTransactionsTotal.ts
@@ -1,4 +1,3 @@
-import { Iva } from '@prisma/client';
 import { calculateType } from '../types';
 
 // This function helps calculate the subtotal for a single item in a detailsPurchase or detailsSale.
@@ -32,7 +31,7 @@ export const calculateTotal = ({ subtotal, discount, iva }: calculateType) => {
 // This function calculates the total of a transaction (purchase or sale) from item details, considering iva and a general discount.
 export const calculateTotalFromDetails = (
     value: calculateType[],
-    iva: Iva,
+    iva: number,
     discount: number,
 ) => {
     // Calculate the subtotal for each item (product) using its price and discount.
@@ -51,7 +50,7 @@ export const calculateTotalFromDetails = (
     const calculatedTotal = calculateTotal({
         subtotal: subtotalDetailsGeneral,
         discount: discount,
-        iva: iva.rate,
+        iva: iva,
     });
 
     return calculatedTotal;
