0a4baf1c2c0004b941007dda178fb2fa78f89a7c
Antonio-Conrado <jconradolopez19@gmail.com>
2025-06-04 16:32:37 -0600

    updated routes

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,6 @@ import category from './routes/category';
 import supplier from './routes/supplier';
 import product from './routes/product';
 import businessData from './routes/businessData';
-import iva from './routes/iva';
 import paymentMethod from './routes/paymentMethod';
 import purchase from './routes/purchase';
 
@@ -19,7 +18,6 @@ const routes = (app: Express) => {
     app.use('/api/v1/suppliers', supplier);
     app.use('/api/v1/products', product);
     app.use('/api/v1/businessData', businessData);
-    app.use('/api/v1/iva', iva);
     app.use('/api/v1/payment-methods', paymentMethod);
     app.use('/api/v1/purchases', purchase);
 };
