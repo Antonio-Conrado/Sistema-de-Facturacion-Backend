000511498f2618dda22353738cd1e0967edd41c9
Antonio-Conrado <jconradolopez19@gmail.com>
2025-07-12 16:33:38 -0600

    middleware to validate sales fields

--- src/middlewares/transactionsValidation.ts
+++ src/middlewares/transactionsValidation.ts
@@ -15,10 +15,18 @@ export const purchaseValidation = async (
             )
             .run(req),
 
+        body('invoiceNumber')
+            .toInt()
+            .isInt({ min: 1 })
+            .withMessage(
+                'El campo número de factura es obligatorio y debe ser un número entero',
+            )
+            .run(req),
+
         // validation for "detailsPurchases"
         body('detailsPurchases')
             .isArray()
-            .withMessage('El campo detalles de venta debe ser un array')
+            .withMessage('El campo detalles de compra debe ser un array')
             .run(req),
 
         body('detailsPurchases.*.storedProductsId')
@@ -37,17 +45,17 @@ export const purchaseValidation = async (
 
         body('detailsPurchases.*.purchasePrice')
             .toFloat()
-            .isFloat({ min: 0 })
+            .isFloat({ min: 1 })
             .withMessage(
-                'El campo precio de compra debe ser un número mayor o igual a 0',
+                'El campo precio de compra debe ser un número mayor o igual a 1',
             )
             .run(req),
 
         body('detailsPurchases.*.salePrice')
             .toFloat()
-            .isFloat({ min: 0 })
+            .isFloat({ min: 1 })
             .withMessage(
-                'El campo precio de venta debe ser un número mayor o igual a 0',
+                'El campo precio de venta debe ser un número mayor o igual a 1',
             )
             .custom((salePrice, { req, path }) => {
                 // Extract the index from the path (example "detailsPurchases.0.salePrice" → 0)
@@ -107,7 +115,65 @@ export const ivaValidation = async (
     await body('iva')
         .toInt()
         .isIn([0, 15])
-        .withMessage('El campo Iva es obligatorio y debe ser un número entero')
+        .withMessage('El campo Iva es obligatorio y debe ser 0% o 15%')
         .run(req),
         next();
+};
+
+// sale
+
+export const saleValidation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    await Promise.all([
+        body('paymentMethodId')
+            .toInt()
+            .isInt({ min: 1 })
+            .withMessage(
+                'El campo método de pago es obligatorio y debe ser un número entero',
+            )
+            .run(req),
+
+        // validation for "detailsSales"
+        body('detailsSales')
+            .isArray()
+            .withMessage('El campo detalles de venta debe ser un array')
+            .run(req),
+
+        body('detailsSales.*.storedProductsId')
+            .toInt()
+            .isInt({ min: 1 })
+            .withMessage('El campo del producto debe ser un número entero')
+            .run(req),
+
+        body('detailsSales.*.amount')
+            .toInt()
+            .isInt({ min: 1 })
+            .withMessage(
+                'El campo cantidad debe ser un número entero mayor o igual a 1',
+            )
+            .run(req),
+
+        body('detailsSales.*.price')
+            .toFloat()
+            .isFloat({ min: 1 })
+            .withMessage(
+                'El campo precio de venta debe ser un número mayor o igual a 1',
+            )
+
+            .run(req),
+
+        body('detailsSales.*.discount')
+            .optional()
+            .toFloat()
+            .isFloat({ min: 0, max: 50 })
+            .withMessage(
+                'El campo descuento debe tener un rango de 0% - 50% para ser válido',
+            )
+            .run(req),
+    ]);
+
+    next();
 };
