f2e29fb3e92c9feed1c6c8e6b8fc5583c589a7c6
Antonio-Conrado <jconradolopez19@gmail.com>
2025-07-12 16:34:27 -0600

    Added user existence check to validateRole middleware

--- src/middlewares/validateRole.ts
+++ src/middlewares/validateRole.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { prisma } from '../config/db';
 export const validateRole = (roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user || !req.user.id) {
+            const error = new Error('Usuario no autenticado');
+            res.status(401).json({ error: error.message });
+            return;
+        }
+
         const user = await prisma.users.findUnique({
             where: { id: req.user.id },
             include: {
