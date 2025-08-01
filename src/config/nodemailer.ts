876ce41948539ccb9184e30806945037804a1d3f
Antonio-Conrado <jconradolopez19@gmail.com>
2025-08-01 09:54:53 -0600

    updated config nodemailer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,17 +1,18 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 const config = () => {
     return {
+        service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_HOST,
         port: +process.env.EMAIL_PORT,
         secure: false,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            pass: process.env.EMAIL_PASS,
         },
         tls: {
-            ciphers: "TLSv1.2,TLSv1.3",
+            ciphers: 'TLSv1.2,TLSv1.3',
         },
-    }
-}
+    };
+};
 export const transport = nodemailer.createTransport(config());
