82ba65e5d5d2a1634a8f00e1e889d76f16dc8776
Antonio-Conrado <jconradolopez19@gmail.com>
2026-09-01 06:26:30 -0600

    updated config nodemailer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const config = () => {
+    console.log('EMAIL_HOST at load time:', process.env.EMAIL_HOST);
     return {
         service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_HOST,
@@ -15,4 +16,11 @@ const config = () => {
         },
     };
 };
-export const transport = nodemailer.createTransport(config());
+let _transport: nodemailer.Transporter | null = null;
+
+export const getTransport = () => {
+    if (!_transport) {
+        _transport = nodemailer.createTransport(config());
+    }
+    return _transport;
+};
--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,37 +1,37 @@
-import { transport } from "../config/nodemailer"
+import { getTransport } from '../config/nodemailer';
 
 type EmailType = {
-    name: string
-    email: string
-    token: string
-}
+    name: string;
+    email: string;
+    token: string;
+};
 
 export class AuthEmail {
     static sendConfirmatioEmail = async (user: EmailType) => {
-        await transport.sendMail({
-            from: 'CashTracker <admin@crashtracker.com',
+        await getTransport().sendMail({
+            from: 'NicaFact <admin@nicafact.com',
             to: user.email,
-            subject: 'crachtracker - confirma tu cuenta',
+            subject: 'NicaFact - confirma tu cuenta',
             html: `
                 <p>Hola: ${user.name}, has creado tu cuenta</p>
                 <p>Haz click en el siguiente enlace para confirmar tu cuenta </p>
                 <a href='${process.env.FRONTEND_URL}/confirmar-cuenta'>Confirmar cuenta</a>
                 <p>Ingresa el codigo: <b>${user.token}</b></p>
-            `
-        })
-    }
+            `,
+        });
+    };
 
     static sendPasswordResetToken = async (user: EmailType) => {
-        await transport.sendMail({
-            from: 'CashTracker <admin@crashtracker.com',
+        await getTransport().sendMail({
+            from: 'NicaFact <admin@nicafact.com',
             to: user.email,
-            subject: 'crachtracker - restablece tu password',
+            subject: 'NicaFact - restablece tu password',
             html: `
                 <p>Hola: ${user.name}, has solicitado restablecer tu cuenta</p>
                 <p>Haz click en el siguiente enlace para restablecer tu passwor </p>
                 <a href='${process.env.FRONTEND_URL}/resetear-password'>Restablecer password</a>
                 <p>Ingresa el codigo: <b>${user.token}</b></p>
-            `
-        })
-    }
+            `,
+        });
+    };
 }
